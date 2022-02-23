3961e06452e4ac8d27b48397fd8defd24ff5fbc6
SpiidiK <97437884+SpiidiK@users.noreply.github.com>
2022-02-23 12:03:01 +0200

    matka-app
    parandused. Kõik toimib

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const matk1 = {
   id: 0,
   nimetus: "Matk kõrbes",
   kirjeldus: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ipsa sed quod reprehenderit ipsum veritatis accusamus nesciunt nisi saepe deserunt quaerat, sunt alias, voluptates illum aspernatur! Facilis qui sunt cum!",
-  pildiUrl: "./assets/uudis1.jpg",
+  pildiUrl: "/assets/uudis1.jpg",
   osalejad: ['mati@matkaja.ee', 'kati@matkajad.ee']
 }
 const matk2 = {
   id: 1,
   nimetus: "Matk Piusas",
   kirjeldus: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam ipsa sed quod reprehenderit ipsum veritatis accusamus nesciunt nisi saepe deserunt quaerat, sunt alias, voluptates illum aspernatur! Facilis qui sunt cum!",
-  pildiUrl: "./assets/piusa.jpg",
+  pildiUrl: "/assets/piusa.jpg",
   osalejad: ['klaabu@suurmeri.ee' ]
 }
 
@@ -22,10 +22,10 @@ const matkad = [
   matk1, 
   matk2,
   {
-    id: 3,
+    id: 2,
     nimetus: "Matk  mägedes",
     kirjeldus: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore minima, nemo tempora rem corrupti sequi architecto eius fuga magnam temporibus dolor quam et, omnis illum officia fugit voluptatum perspiciatis! Est.",
-    pildiUrl: "./assets/uudis2.jpg",
+    pildiUrl: "/assets/uudis2.jpg",
     osalejad: []  
   }
 ]
@@ -36,7 +36,7 @@ const uudis1 = {
   nimetus: 'Juhend, kuidas matkata maroko kõrvetavat kõrbes',
   kommentaarid: '(12)',
   kirjeldus: 'Maroko pakub suhteliselt lihtsat juurdepääsu suurepärasele Sahara kõrbele, mis on suurim kuum kõrb maa peal. Maroko külastades on tohutu luited, näiliselt lõputud liivarandused, ettearvamatu kliima, vahuveinid oaasid ja vastupidav taimestik, ebasoodsam ja äärmuslik ala on seikluste otsijatele mõeldud magnetit.',
-  pildiUrl: "./assets/uudis1.jpg",
+  pildiUrl: "/assets/uudis1.jpg",
   kommentaarid: '(12)',
   
 }
@@ -45,7 +45,7 @@ const uudis2 = {
   nimetus: 'Konservipurgiga mõmmide keskel  ',
  
   kirjeldus: 'Automatk Ameerika Ühendriikides kuulub paljude inimeste reisiunistuste nimekirja. Mööda laia maanteed kulgemine on üheaegselt põnev ja lõõgastav. Põnev eelkõige seetõttu, et USA on tohutult suur ja sellele suurele maalapile mahub palju vingeid kohti. Vaid paaritunnise autosõiduga võib vahetada ranna lumise mäetipu või kõrbe lopsaka metsa vastu.',
-  pildiUrl: "./assets/uudis2.jpg",
+  pildiUrl: "/assets/uudis2.jpg",
   kommentaarid: '(22)',
   
 }
@@ -54,7 +54,7 @@ const uudis3 = {
   nimetus: 'Matkavarustuse valimise ABC',
  
   kirjeldus: 'Matkavarustuse soetamist tasub alustada jalanõudest. Eesti tingimustes on parim pehme tallaga jalats, et oleks hea liikuda. Jäiga tallaga jalanõud on mõeldud kivistesse Alpidesse tehniliseks ronimiseks. Jalanõu peab eelkõige olema mugav. Kas see riietega kokku sobib, pole kogenud matkaja kinnitusel oluline.',
- pildiUrl: "./assets/uudis3.jpg",
+ pildiUrl: "/assets/uudis3.jpg",
  kommentaarid: '(3)',
 }
 const uudis4 = {
@@ -62,7 +62,7 @@ const uudis4 = {
   nimetus: 'Ellujäämisnipid talvel telkimiseks',
   
   kirjeldus: 'Põhja-Rootsis kelgukoerte giidina ja 2019. aastal 300-kilomeetrise Fjällräven Polar polaarmatka kelgukoertega läbinud Maiu Lünekund jagab hüva nõu, kuidas öö telgis keset paksu ja lumist metsa turvaliselt mööda saata. 1) Ära higista, ära saa märjaks „Don’t sweat, don’t get wet – ära higista, ära saa ',
-  pildiUrl: "./assets/uudis4.jpg",
+  pildiUrl: "/assets/uudis4.jpg",
   kommentaarid: '(11)',
 }
 const uudis5 = {
@@ -70,7 +70,7 @@ const uudis5 = {
   nimetus: 'Lahtise tulega tuleb olla ettevaatlik',
   
   kirjeldus: 'Näiteks põhjustab kodus sageli tulekahju grillimine või lõkke tegemine. Seetõttu on nende tegevuste juures oluline järgida ohutusreegleid nii eramajas kui ka kortermajas. Vaata ohutu grillimise ja lõkke tegemise juhendit SIIT',
-  pildiUrl: "./assets/uudis5.jpg",
+  pildiUrl: "/assets/uudis5.jpg",
   kommentaarid: '(8)',
 }
 const uudis6 = {
@@ -99,7 +99,5 @@ function naitaRegistreerimist(req, res) {
   .get('/', (req, res) => res.render('pages/index', {matkad: matkad}))
   .get('/uudised', (req, res) => res.render('pages/uudised' , {uudised: uudised}))
   .get('/kontakt', (req, res) => res.render('pages/kontakt'))
-  .get('/registreerumine', (req, res) => res.render('pages/registreerumine'))
   .get('/registreerumine/:matk', naitaRegistreerimist)
-
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
